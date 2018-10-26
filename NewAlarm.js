90a25bcde35f494a0daab3504b3782f7bf66b3e9
AbdeLaziz EL ARASSI <lailaelmanssour@gmail.com>
2018-10-26 17:04:28 +0100

    Fixed UI issues & Adding SLIDER

--- NewAlarm.js
+++ NewAlarm.js
@@ -6,19 +6,59 @@ export default class NewAlarm extends Component {
     return (
       <React.Fragment>
         <View style={styles.container}>
-          <Text style={{ fontSize: 20, marginBottom: 30, color: "gray" }}>
+          <Text
+            style={{ fontSize: 32, marginBottom: 30, color: "rgba(0,0,0,.4)" }}
+          >
             New Alarm
           </Text>
-          <Text style={{ fontSize: 46, marginBottom: 30, color: "gray" }}>
+          <Text
+            style={{ fontSize: 46, marginBottom: 40, color: "rgba(0,0,0,.4)" }}
+          >
             12:30
           </Text>
+          <View style={styles.hourWrapper}>
+            <Text style={{ color: "rgba(0,0,0,.3)", fontSize: 22 }}>00</Text>
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <Text style={{ color: "rgba(0,0,0,.3)", fontSize: 22 }}>12</Text>
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <Text style={{ color: "rgba(0,0,0,.3)", fontSize: 22 }}>23</Text>
+          </View>
           <Slider
             step={1}
             maximumValue={23}
             value={0}
-            disabled="true"
             onSlidingComplete={null}
-            style={{ width: 200, height: 20, padding: 30 }}
+            style={{
+              width: "80%",
+              height: 20,
+              padding: 30
+            }}
+          />
+          <View style={styles.hourWrapper}>
+            <Text style={{ color: "rgba(0,0,0,.3)", fontSize: 22 }}>00</Text>
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <Text style={{ color: "rgba(0,0,0,.3)", fontSize: 22 }}>12</Text>
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <View style={{ width: 2, height: 15, backgroundColor: "gray" }} />
+            <Text style={{ color: "rgba(0,0,0,.3)", fontSize: 22 }}>59</Text>
+          </View>
+          <Slider
+            step={1}
+            maximumValue={23}
+            value={0}
+            onSlidingComplete={null}
+            style={{
+              width: "80%",
+              height: 20,
+              padding: 30
+            }}
           />
           <View style={styles.roundedContainer}>
             <Item title="M" />
@@ -54,21 +94,24 @@ const Item = props => {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    padding: 10,
+    flexGrow: 1,
+    justifyContent: "space-around",
     alignItems: "center"
   },
   roundedContainer: {
-    width: 300,
-    height: 100,
-    backgroundColor: "yellow",
+    width: "80%",
+    justifyContent: "center",
     flexDirection: "row",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    alignContent: "center",
+    marginBottom: 30
   },
   roundedView: {
     justifyContent: "center",
     alignItems: "center",
-    width: 30,
-    height: 30,
+    width: 48,
+    height: 48,
     borderWidth: 1,
     borderStyle: "solid",
     borderColor: "gray",
@@ -81,5 +124,11 @@ const styles = StyleSheet.create({
     width: "100%",
     flexDirection: "row",
     justifyContent: "space-around"
+  },
+  hourWrapper: {
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   }
 });
